ab4787b1f369332fb9fccc9e52e147ccd39d2b01
Rohit <rohit.t@webkorps.com>
2022-09-29 01:52:19 +0530

    bootstrap devise fontawesome navbar

--- app/controllers/static_public_controller.rb
+++ app/controllers/static_public_controller.rb
@@ -1,5 +1,5 @@
 class StaticPublicController < ApplicationController
-  skip_before_action :authenticate_user!, only:%i[landing_page privacy]
+  skip_before_action :authenticate_user!
   def landing_page
   end
 
